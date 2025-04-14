3c318469ad02c0c16668c2d48e22f45405568bde
quartzs2 <csh001231@gmail.com>
2025-04-15 00:51:33 +0900

    feat: localStorage 사용

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -3,8 +3,11 @@ import styles from "../styles/TodoList.module.css";
 import TodoInput from "./TodoInput";
 import TodoItem from "./TodoItem";
 import classNames from "classnames";
+import { TODO_LIST_KEY } from "../constants/storageKey";
 
 function todoReducer(todos, action) {
+  let newTodos;
+
   switch (action.type) {
     case "add_todo": {
       const newTodo = {
@@ -12,24 +15,39 @@ function todoReducer(todos, action) {
         content: action.content,
         isDone: false,
       };
-      return [...todos, newTodo];
+      newTodos = [...todos, newTodo];
+
+      break;
     }
     case "update_todo": {
-      return todos.map((todo) =>
+      newTodos = todos.map((todo) =>
         todo.id === action.id ? { ...todo, content: action.content, isDone: action.isDone } : todo
       );
+      break;
     }
     case "delete_todo": {
-      return todos.filter((todo) => todo.id !== action.id);
+      newTodos = todos.filter((todo) => todo.id !== action.id);
+      break;
     }
     default: {
       throw Error("Unknown action: " + action.type);
     }
   }
+
+  localStorage.setItem(TODO_LIST_KEY, JSON.stringify(newTodos));
+  return newTodos;
 }
 
+const loadInitialTodos = () => {
+  const savedTodos = localStorage.getItem(TODO_LIST_KEY);
+  if (savedTodos) {
+    return JSON.parse(savedTodos);
+  }
+  return [];
+};
+
 const TodoList = ({ className }) => {
-  const [todos, dispatch] = useReducer(todoReducer, []);
+  const [todos, dispatch] = useReducer(todoReducer, null, loadInitialTodos);
 
   return (
     <section className={classNames(className)}>
